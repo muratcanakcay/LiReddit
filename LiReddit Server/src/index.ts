38fd2b109ef036bdc9dc8126a3b44ca26f6d89a5
Muratcan Akçay <muratcanakcay@gmail.com>
2024-06-08 01:47:52 +0200

    Implement UserResolver and add register mutation

--- LiReddit Server/src/index.ts
+++ LiReddit Server/src/index.ts
@@ -12,6 +12,7 @@ import { ApolloServer } from "apollo-server-express"
 import { buildSchema } from "type-graphql"
 import { HelloResolver } from "./resolvers/hello"
 import { PostResolver } from './resolvers/post'
+import { UserResolver } from './resolvers/user'
 
 const main = async () => {
   const orm = await MikroORM.init(microConfig)
@@ -25,7 +26,7 @@ const main = async () => {
 
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
-      resolvers: [HelloResolver, PostResolver],
+      resolvers: [HelloResolver, PostResolver, UserResolver],
       validate: false
     }),
     context: () => ({ em: orm.em }) // context is shared with  all resolvers
