89d0be8426f34b9f5780894f22622444d4834d14
Muratcan Akçay <muratcanakcay@gmail.com>
2024-06-07 04:54:50 +0200

    add post query, add createPost mutation

--- LiReddit Server/src/resolvers/post.ts
+++ LiReddit Server/src/resolvers/post.ts
@@ -1,11 +1,27 @@
 import { Post } from "../entities/Post";
 import { MyContext } from "src/types";
-import { Ctx, Query, Resolver } from "type-graphql";
+import { Arg, Ctx, Int, Mutation, Query, Resolver } from "type-graphql";
 
 @Resolver()
 export class PostResolver {
   @Query(() => [Post])
   posts(@Ctx() { em }: MyContext): Promise<Post[]> {    
     return em.find(Post, {})
+  }
+
+  @Query(() => Post, {nullable: true})
+  post(
+    @Arg('id', () => Int) id: number,  // 'id' is just a name for using in GraphQL schema, id is the actual field in database
+    @Ctx() { em }: MyContext): Promise<Post | null> {    
+    return em.findOne(Post, { id })
+  }
+
+  @Mutation(() => Post)
+  async createPost(
+    @Arg('title', () => String) title: string,
+    @Ctx() { em }: MyContext): Promise<Post> {    
+    const post = em.create(Post, { title })
+    await em.persistAndFlush(post)
+    return post
   }
 }
