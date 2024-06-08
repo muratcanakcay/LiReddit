739887222af0981d7f89ac5a35797e3870cde9aa
Muratcan Akçay <muratcanakcay@gmail.com>
2024-06-08 02:23:09 +0200

    add login mutation

--- LiReddit Server/src/resolvers/user.ts
+++ LiReddit Server/src/resolvers/user.ts
@@ -1,15 +1,32 @@
 import { User } from "../entities/User";
 import { MyContext } from "src/types";
-import { Arg, Ctx, Field, InputType, Mutation, Resolver } from "type-graphql";
+import { Arg, Ctx, Field, InputType, Mutation, ObjectType, Resolver } from "type-graphql";
 import argon2 from "argon2"
 
 
-@InputType() 
+@InputType()  // InputType are used for arguments
 class UsernamePasswordInput {
   @Field()
   username: string
   @Field(() => String) // can set type explicitly, or let typescript infer it
   password: string
+}
+
+@ObjectType()
+class FieldError {
+  @Field()
+  field: string // which field the error is about
+  @Field()
+  message: string // error message
+}
+
+@ObjectType() // ObjectTypes are returned from Queries and Mutations
+class UserResponse {
+  @Field(() => [FieldError], { nullable: true})
+  errors?: FieldError[]
+
+  @Field(() => User, { nullable: true})
+  user?: User
 }
 
 @Resolver()
@@ -24,5 +41,37 @@ export class UserResolver {
     await em.persistAndFlush(user)
 
     return user
+  }
+
+  @Mutation(() => UserResponse)
+  async login(
+    @Arg('options') options: UsernamePasswordInput, //  let typescript infer type UsernamePasswordInput
+    @Ctx() { em } : MyContext
+  ) : Promise<UserResponse> {
+    const user = await em.findOne(User, {username: options.username})
+    if (!user) {
+      return {
+        errors: [
+          {
+            field: "username",
+            message: "That username does not exist"
+          }
+        ]
+      }
+    }
+    
+    const isPasswordValid = await argon2.verify(user.password, options.password)
+    if (!isPasswordValid) {
+      return {
+        errors: [
+          {
+            field: "password",
+            message: "Incorrect password"
+          }
+        ]
+      }
+    }
+
+    return { user }
   }
 }
