5ffc72bc9820722ba8549d8031317a80a1ee61f1
Muratcan Akçay <muratcanakcay@gmail.com>
2024-06-09 22:45:38 +0200

    Implement urql to send request to GraphQL server + fix CORS error

--- lireddit-server/src/index.ts
+++ lireddit-server/src/index.ts
@@ -1,65 +1,75 @@
-import 'reflect-metadata'
-import { MikroORM } from "@mikro-orm/core"
-import { __prod__ } from "./constants"
+import "reflect-metadata";
+import { MikroORM } from "@mikro-orm/core";
+import { __prod__ } from "./constants";
 // import { Post } from "./entities/Post"
-import microConfig from "./mikro-orm.config"
-import express from "express"
-import { ApolloServer } from "apollo-server-express"
-import { buildSchema } from "type-graphql"
-import { HelloResolver } from "./resolvers/hello"
-import { PostResolver } from './resolvers/post'
-import { UserResolver } from './resolvers/user'
-import connectRedis from "connect-redis"
-import session from "express-session"
-import redis from "redis"
-import { MyContext } from './types'
+import microConfig from "./mikro-orm.config";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
+import { PostResolver } from "./resolvers/post";
+import { UserResolver } from "./resolvers/user";
+import connectRedis from "connect-redis";
+import session from "express-session";
+import redis from "redis";
+import { MyContext } from "./types";
+import cors from "cors";
 
 const main = async () => {
-  const orm = await MikroORM.init(microConfig)
-  await orm.getMigrator().up() // run migration
-  
-  const app = express()
+  const orm = await MikroORM.init(microConfig);
+  await orm.getMigrator().up(); // run migration
 
-  const RedisStore = connectRedis(session)
-  const redisClient = redis.createClient()
+  const app = express();
+
+  const RedisStore = connectRedis(session);
+  const redisClient = redis.createClient();
+
+  // define CORS to avoid CORS errors
+  app.use(
+    cors({
+      origin: "http://localhost:3000",
+      credentials: true,
+    })
+  );
 
   // Initialize session storage before Apollo since it will be used from inside Apollo.
   app.use(
     session({
-      name: 'qid', // cookie name
+      name: "qid", // cookie name
       store: new RedisStore({
         client: redisClient,
         disableTTL: true, // keep session alive forever
-        disableTouch: true, // disable TTL reset at every touch 
+        disableTouch: true, // disable TTL reset at every touch
       }),
       cookie: {
         maxAge: 1000 * 60 * 60 * 24 * 365 * 10, // 10 years
         httpOnly: true, // prevent accessing the cookie in the JS code in the frontend
         sameSite: "lax",
-        secure: __prod__ // cookie only works in https
+        secure: __prod__, // cookie only works in https
       },
       saveUninitialized: false,
-      secret: "asdfasdfasdf",  // used to sign cookie - should actually be hidden in an env variable 
-      resave: false, 
-    }),
-  )
+      secret: "asdfasdfasdf", // used to sign cookie - should actually be hidden in an env variable
+      resave: false,
+    })
+  );
 
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
       resolvers: [HelloResolver, PostResolver, UserResolver],
-      validate: false
+      validate: false,
     }),
-    context: ({ req, res } : MyContext) => ({ em: orm.em, req, res }) // context is shared with all resolvers
-  })
+    context: ({ req, res }: MyContext) => ({ em: orm.em, req, res }), // context is shared with all resolvers
+  });
 
-  apolloServer.applyMiddleware({ app })
-
-  
+  apolloServer.applyMiddleware({
+    app,
+    cors: false,
+  });
 
   app.listen(4000, () => {
-    console.log('server started on localhost:4000')
-  })
-  
+    console.log("server started on localhost:4000");
+  });
+
   // const post = orm.em.create(Post, { title: "my first post" }) // the Post class is used to create the missing fields
   // await orm.em.persistAndFlush(post)
   // console.log("--------------sql 2----------------")
@@ -67,10 +77,8 @@ const main = async () => {
 
   // const posts = await orm.em.find(Post, {})
   // console.log(posts)
-}
+};
 
 main().catch((err) => {
-  console.log(err)
-})
-
-
+  console.log(err);
+});
--- lireddit-web/src/pages/register.tsx
+++ lireddit-web/src/pages/register.tsx
@@ -1,25 +1,40 @@
 import React from "react";
-import { Field, Form, Formik } from "formik";
-import {
-  FormControl,
-  FormLabel,
-  Input,
-  FormErrorMessage,
-  Button,
-  Box,
-} from "@chakra-ui/react";
+import { Form, Formik } from "formik";
+import { Button, Box } from "@chakra-ui/react";
 import { Wrapper } from "../components/Wrapper";
 import { InputField } from "../components/InputField";
+import { useMutation } from "urql";
 
 interface registerProps {}
 
 const Register: React.FC<registerProps> = ({}) => {
+  // this is the mutation copied from GraphQL PlayGround. We pass it to useMutation and use the returned function in handlesubmit
+  const REGISTER_MUT = `
+  mutation Register ($username: String!, $password: String!){
+    register(options: { username: $username, password: $password  }) {
+      errors {
+        field
+        message
+      }
+      user {
+        id
+        username
+        createdAt
+        updatedAt
+      }
+    }
+  }
+  `;
+
+  const [, /*first object can be ommitted for now*/ register] =
+    useMutation(REGISTER_MUT);
+
   return (
     <Wrapper variant="small">
       <Formik
         initialValues={{ username: "", password: "" }}
         onSubmit={(values) => {
-          console.log(values);
+          return register(values);
         }}
       >
         {({ isSubmitting }) => (
