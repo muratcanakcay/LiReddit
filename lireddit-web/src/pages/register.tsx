24560da31822af1062a2616a7c531ba89fce61b5
Muratcan Akçay <muratcanakcay@gmail.com>
2024-06-10 01:06:44 +0200

    route to homepage after successful registration

--- lireddit-web/src/pages/register.tsx
+++ lireddit-web/src/pages/register.tsx
@@ -5,10 +5,12 @@ import { Wrapper } from "../components/Wrapper";
 import { InputField } from "../components/InputField";
 import { useRegisterMutation } from "../generated/graphql";
 import { toErrorMap } from "../utils/toErrorMap";
+import { useRouter } from "next/router";
 
 interface registerProps {}
 
 const Register: React.FC<registerProps> = ({}) => {
+  const router = useRouter();
   const [, register] = useRegisterMutation();
 
   return (
@@ -19,6 +21,9 @@ const Register: React.FC<registerProps> = ({}) => {
           const response = await register(values);
           if (response.data?.register.errors) {
             setErrors(toErrorMap(response.data.register.errors));
+          } else if (response.data?.register.user) {
+            // register success, route to homepage
+            router.push("/");
           }
         }}
       >
