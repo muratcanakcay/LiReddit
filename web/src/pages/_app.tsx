4e5bd9f29dd78b5e324314785c9c8720436fb7a2
Muratcan Akçay <muratcanakcay@gmail.com>
2024-06-11 05:27:31 +0200

    implement cache updating via @urql/graphcache
    There's an error in the graphql dependencies, which can be circumvented by appending ".mjs" to the file imports mentioned in the error message.  I spent hours trying to fix the packages but they're 4 years old and I won't bother more.

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,15 +1,73 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import theme from "../theme";
 import { AppProps } from "next/app";
-import { Provider, createClient, fetchExchange } from "urql";
-import { cacheExchange } from "@urql/exchange-graphcache";
+import { Provider, createClient, dedupExchange, fetchExchange } from "urql";
+import { cacheExchange, Cache, QueryInput } from "@urql/exchange-graphcache";
+import {
+  LoginMutation,
+  MeDocument,
+  MeQuery,
+  RegisterMutation,
+} from "../generated/graphql";
+
+function betterUpdateQuery<Result, Query>(
+  cache: Cache,
+  qi: QueryInput,
+  result: any,
+  updaterFn: (r: Result, q: Query) => Query
+) {
+  return cache.updateQuery(qi, (data) => updaterFn(result, data as any) as any);
+}
 
 const client = createClient({
   url: "http://localhost:4000/graphql",
   fetchOptions: {
     credentials: "include",
   },
-  exchanges: [cacheExchange({}), fetchExchange],
+  exchanges: [
+    dedupExchange,
+    cacheExchange({
+      // this will update the cache everytime the defined mutations are run run
+      updates: {
+        Mutation: {
+          login: (result, args, cache, info) => {
+            // cache.updateQuery({ query: MeDocument }, (data: MeQuery) => { })
+            betterUpdateQuery<LoginMutation, MeQuery>(
+              cache,
+              { query: MeDocument },
+              result,
+              (r, q) => {
+                if (r.login.errors) {
+                  return q; // return the current query if there's error
+                } else {
+                  return {
+                    me: r.login.user, // return the user info received from successful login
+                  };
+                }
+              }
+            );
+          },
+          register: (result, args, cache, info) => {
+            betterUpdateQuery<RegisterMutation, MeQuery>(
+              cache,
+              { query: MeDocument },
+              result,
+              (r, q) => {
+                if (r.register.errors) {
+                  return q; // return the current query if there's error
+                } else {
+                  return {
+                    me: r.register.user, // return the user info received from successful register
+                  };
+                }
+              }
+            );
+          },
+        },
+      },
+    }),
+    fetchExchange,
+  ],
 });
 
 function MyApp({ Component, pageProps }: AppProps) {
