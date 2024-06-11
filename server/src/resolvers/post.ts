be8774536d5aa43f9e7af5d76ba9dabcd86bdc91
Muratcan Akçay <muratcanakcay@gmail.com>
2024-06-12 01:51:35 +0200

    implement ssr for login and register pages

--- server/src/resolvers/post.ts
+++ server/src/resolvers/post.ts
@@ -1,68 +1,67 @@
 import { Post } from "../entities/Post";
 import { MyContext } from "src/types";
 import { Arg, Ctx, Int, Mutation, Query, Resolver } from "type-graphql";
+import { sleep } from "../utils/sleep";
 
 @Resolver()
 export class PostResolver {
   @Query(() => [Post])
-  posts(
-    @Ctx() { em }: MyContext
-  ): Promise<Post[]> {    
-      return em.find(Post, {})
-    }
+  async posts(@Ctx() { em }: MyContext): Promise<Post[]> {
+    await sleep(3000); // simulate delay to test csr vs ssr load times
+    return em.find(Post, {});
+  }
 
-  @Query(() => Post, {nullable: true})
+  @Query(() => Post, { nullable: true })
   post(
-    @Arg('id', () => Int) id: number,  // 'id' is just a name for using in GraphQL schema, id is the actual field in database
+    @Arg("id", () => Int) id: number, // 'id' is just a name for using in GraphQL schema, id is the actual field in database
     @Ctx() { em }: MyContext
-  ): Promise<Post | null> {    
-    return em.findOne(Post, { id })
+  ): Promise<Post | null> {
+    return em.findOne(Post, { id });
   }
 
   @Mutation(() => Post)
   async createPost(
-    @Arg('title', () => String) title: string,
+    @Arg("title", () => String) title: string,
     @Ctx() { em }: MyContext
-  ): Promise<Post> {    
-    const post = em.create(Post, { title })
-    
-    await em.persistAndFlush(post)
-    return post
+  ): Promise<Post> {
+    const post = em.create(Post, { title });
+
+    await em.persistAndFlush(post);
+    return post;
   }
 
   @Mutation(() => Post, { nullable: true })
   async updatePost(
-    @Arg('id') id: number, // here we ommitted type declaration in @Arg - type inference works for Int and String
-    @Arg('title', () => String, { nullable: true }) title: string, // here we explicitly set type since we want to make it nullable
+    @Arg("id") id: number, // here we ommitted type declaration in @Arg - type inference works for Int and String
+    @Arg("title", () => String, { nullable: true }) title: string, // here we explicitly set type since we want to make it nullable
     @Ctx() { em }: MyContext
   ): Promise<Post | null> {
-    const post = await em.findOne(Post, { id })
-    
+    const post = await em.findOne(Post, { id });
+
     if (!post) {
-      return null
+      return null;
     }
 
-    if (typeof title !== 'undefined') {
-      post.title = title
-      await em.persistAndFlush(post)
+    if (typeof title !== "undefined") {
+      post.title = title;
+      await em.persistAndFlush(post);
     }
 
-    return post
+    return post;
   }
 
   @Mutation(() => Boolean)
   async deletePost(
-    @Arg('id') id: number,
+    @Arg("id") id: number,
     @Ctx() { em }: MyContext
   ): Promise<boolean> {
-    const post = await em.findOne(Post, { id })
-    
-    if (!post) {
-      return false
-    }
-    
-    await em.nativeDelete(Post, { id })
-    return true
-  }
+    const post = await em.findOne(Post, { id });
 
+    if (!post) {
+      return false;
+    }
+
+    await em.nativeDelete(Post, { id });
+    return true;
+  }
 }
--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -9,9 +9,14 @@ const Index = () => {
     <>
       <NavBar />
       <div>Hello World!</div>
-      {!data ? null : data.posts.map((p) => <div key={p.id}>{p.title}</div>)}
+      <br />
+      {!data ? (
+        <div>Loading...</div>
+      ) : (
+        data.posts.map((p) => <div key={p.id}>{p.title}</div>)
+      )}
     </>
   );
 };
 
-export default withUrqlClient(createUrqlClient)(Index);
+export default withUrqlClient(createUrqlClient, { ssr: true })(Index);
