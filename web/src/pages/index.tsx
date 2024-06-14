4d5bb89fc383f7e6dbc9073aa011b6a6d54a2cdf
Muratcan Akçay <muratcanakcay@gmail.com>
2024-06-14 18:03:56 +0200

    add styling to posts

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -3,18 +3,18 @@ import { Layout } from "../components/Layout";
 import { usePostsQuery } from "../generated/graphql";
 import { createUrqlClient } from "../utils/createUrqlClient";
 import NextLink from "next/link";
-import { Button, Link } from "@chakra-ui/react";
+import { Box, Button, Heading, Link, Stack, Text } from "@chakra-ui/react";
 
 const Index = () => {
   const [{ data }] = usePostsQuery({
     variables: {
-      limit: 20,
+      limit: 10,
     },
   });
   return (
     <>
       <Layout>
-        <Button type="button" color="teal">
+        <Button mb={4} type="button" color="teal">
           <Link as={NextLink} href="/create-post">
             Create Post
           </Link>
@@ -23,7 +23,15 @@ const Index = () => {
         {!data ? (
           <div>Loading...</div>
         ) : (
-          data.posts.map((p) => <div key={p.id}>{p.title}</div>)
+          <Stack spacing={8}>
+            {data.posts.map((p) => (
+              //<div key={p.id}>{p.title}</div>
+              <Box key={p.id} p={5} shadow="md" borderWidth="1px">
+                <Heading fontSize="xl">{p.title}</Heading>
+                <Text>{p.text.slice(0, 150)}...</Text>
+              </Box>
+            ))}
+          </Stack>
         )}
       </Layout>
     </>
