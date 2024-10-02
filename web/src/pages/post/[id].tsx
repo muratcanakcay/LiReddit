b290bb35d7d1091cf0ace267cee2bdffeb680306
Muratcan Akçay <muratcanakcay@gmail.com>
2024-10-02 03:50:09 +0200

    continue post Page implementation

--- web/src/pages/post/[id].tsx
+++ web/src/pages/post/[id].tsx
@@ -4,12 +4,13 @@ import { createUrqlClient } from "../../utils/createUrqlClient";
 import { useRouter } from "next/router";
 import { usePostQuery } from "../../generated/graphql";
 import { Layout } from "../../components/Layout";
+import { Box, Heading } from "@chakra-ui/react";
 
 const Post = ({}) => {
   const router = useRouter();
   const postId =
     typeof router.query.id === "string" ? parseInt(router.query.id) : -1;
-  const [{ data, fetching }] = usePostQuery({
+  const [{ data, error, fetching }] = usePostQuery({
     pause: postId === -1,
     variables: {
       id: postId,
@@ -19,12 +20,29 @@ const Post = ({}) => {
   if (fetching) {
     return (
       <Layout>
-        <div>Loading...</div>
+        <Box>Loading...</Box>
       </Layout>
     );
   }
 
-  return <Layout>{data?.post?.text}</Layout>;
+  if (error) {
+    return <div>error</div>;
+  }
+
+  if (!data?.post) {
+    return (
+      <Layout>
+        <Box>Could not find post...</Box>
+      </Layout>
+    );
+  }
+
+  return (
+    <Layout>
+      <Heading mb={4}>{data.post.title}</Heading>
+      {data.post.text}
+    </Layout>
+  );
 };
 
 export default withUrqlClient(createUrqlClient, { ssr: true })(Post);
