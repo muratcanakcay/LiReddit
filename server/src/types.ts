8d74f3d60b6cd5fa91b9a0b43b527b717068f3a7
Muratcan Akçay <muratcanakcay@gmail.com>
2024-10-05 06:25:39 +0200

    add updootLoader to MyContext type

--- server/src/types.ts
+++ server/src/types.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import { Session, SessionData } from "express-session";
 import { Redis } from "ioredis";
 import { createUserLoader } from "./utils/createUserLoader";
+import { createUpdootLoader } from "./utils/createUpdootLoader";
 interface ExtendedRequest extends Request {
   session: Session &
     Partial<SessionData> &
@@ -13,4 +14,5 @@ export type MyContext = {
   res: Response;
   redis: Redis;
   userLoader: ReturnType<typeof createUserLoader>;
+  updootLoader: ReturnType<typeof createUpdootLoader>;
 };
